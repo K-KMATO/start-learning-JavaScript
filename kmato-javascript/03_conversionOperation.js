00dcc46b491605c18eac55e30d549ddf9fabddb0
kmato <kmato197@gmail.com>
2024-02-09 22:00:02 +0530

    dataType convertion
    understand how dataTypes convertion are work with diffrent datatype values

---
+++ kmato-javascript/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33and"
+
+// console.log(typeof score)
+
+// let scoreString = String(score) //converted number to string
+
+// let scoreBoolean = Boolean (score) //converted number to Boolean
+
+let scoreNum = Number(score) // converted string to Number
+// console.log(scoreNum)
+// console.log(typeof scoreNum)
+
+// "33" --> 33
+// "33and" --> NaN
+// true -->1; false --> 0
+
+let userLoggedIn = "33abs"
+let loggedIn = Boolean(userLoggedIn)
+console.log(loggedIn)
+console.log(typeof loggedIn)
+
+// 1-->True
+// ""--> false
+// "kmato"--> true
+// null--> false
+// "33abs"--> true
