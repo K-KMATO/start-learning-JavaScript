eb889be0244dd2bf5c038cfbb1bee039eaaa1c37
kmato <kmato197@gmail.com>
2024-02-15 02:08:53 +0530

    Create 02_switch-statement.js
    understand the switch statement and how its syntax

---
+++ 01-kmato-javascript/03-kmato-javascript-control-flow/02_switch-statement.js
@@ -0,0 +1,58 @@
+// switch statement sytax
+
+// switch (key) {
+//   case value:
+    
+//     break;
+
+//   default:
+//     break;
+// }
+
+//swith statement with the number datatypes
+
+const day = 7
+ switch(day){
+  case 1:
+    console.log("sunday");
+    break;
+  case 2:
+    console.log("monday");
+    break;
+  case 3:
+    console.log("tuesday");
+    break;
+  case 4:
+    console.log("wednusday");
+    break;
+  case 5:
+    console.log("thursday");
+    break;
+  case 6:
+    console.log("friday");
+    break;
+  case 7:
+    console.log("saturday");
+    break;
+    default:
+      console.log("default day");
+      break;
+ }
+
+ //switch statment string dataTypes
+
+ const month = "feb"
+ switch(month){
+  case "jan":
+    console.log("january");
+    break;
+  case "feb":
+    console.log("february");
+    break;
+  case "march":
+    console.log("march");
+    break;
+    default:
+      console.log("default month");
+      break;
+ }
